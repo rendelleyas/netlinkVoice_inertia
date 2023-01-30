1c37611bf5b3c10cba76dfeed2bce5bc5e7c9e8b
unknown <rendell.eyas@gmail.com>
2023-01-30 15:49:11 +0800

    Section17: Pagination, added pagination compoenent, paginate fetching with through() method, added table design

--- resources/js/Pages/Users.vue
+++ resources/js/Pages/Users.vue
@@ -4,14 +4,38 @@
         Users
     </h1>
 
-    <ul>
+    <!-- <ul>
         <li v-for="user in users" :key="user.id" v-text="user.name" />
-    </ul>
+    </ul> -->
+    <!-- component -->
+    <div class="overflow-x-auto mt-6">
+        <div class="bg-white shadow-md rounded">
+            <table class="min-w-max w-full table-auto">
+                <tbody class="text-gray-600 text-sm font-light">
+                    <tr class="border-b border-gray-200 hover:bg-gray-100" v-for="user in users.data" :key="user.id">
+                        <td class="py-3 px-6 text-left">
+                            <div class="flex items-center">
+                                <span>{{user.name}}</span>
+                            </div>
+                        </td>
+                        <td class="py-3 px-6 text-center">
+                            <Link :href="`/users/${user.id}/edit`" class="text-indigo-500 font-bold">
+                                Edit
+                            </Link>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+    <!-- Paginate -->
+    <Pagination :links="users.links" class="mt-6"/>
+    
 </template>
-<script>
-    export default {
-        props: {
-            users: String,
-        },
-    }
+<script setup>
+    import Pagination from '../Shared/Pagination.vue'
+    defineProps({
+        users: Object,
+    })
+   
 </script>
